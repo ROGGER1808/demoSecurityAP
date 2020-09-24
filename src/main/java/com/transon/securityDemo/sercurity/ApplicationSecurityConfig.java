6a8a563701c683c5800e72ddc987b91c5a0dcfa0
rogger1808 <genson1808@gmail.com>
2020-09-24 11:17:19 +0700

    update v8

--- src/main/java/com/transon/securityDemo/sercurity/ApplicationSecurityConfig.java
+++ src/main/java/com/transon/securityDemo/sercurity/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.transon.securityDemo.sercurity;
 
+import com.google.common.collect.ImmutableList;
 import com.transon.securityDemo.auth.MyUserDetailService;
 import com.transon.securityDemo.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
@@ -79,10 +80,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
 
+
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(ImmutableList.of("*"));
+        configuration.setExposedHeaders(ImmutableList.of("X-Auth-Token","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 }
