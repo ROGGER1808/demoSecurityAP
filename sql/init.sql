95e150ac0d1a07fc4b8c2c32fc3c3ad940927f67
rogger1808 <genson1808@gmail.com>
2020-09-26 17:28:13 +0700

    update v14

--- sql/init.sql
+++ sql/init.sql
@@ -1,24 +1,28 @@
-REATE TABLE `department` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_at` datetime(6) DEFAULT NULL,
+CREATE TABLE `department` (
+  `id` bigint NOT NULL,
+  `created_at` datetime DEFAULT NULL,
   `created_by` varchar(255) DEFAULT NULL,
   `is_active` bit(1) DEFAULT NULL,
-  `updated_at` datetime(6) DEFAULT NULL,
+  `modified_at` datetime DEFAULT NULL,
+  `modified_by` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   `updated_by` varchar(255) DEFAULT NULL,
   `name` varchar(250) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_1t68827l97cwyxo9r1u6t4p7d` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `employee` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_at` datetime(6) DEFAULT NULL,
+  `id` bigint NOT NULL,
+  `created_at` datetime DEFAULT NULL,
   `created_by` varchar(255) DEFAULT NULL,
   `is_active` bit(1) DEFAULT NULL,
-  `updated_at` datetime(6) DEFAULT NULL,
+  `modified_at` datetime DEFAULT NULL,
+  `modified_by` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   `updated_by` varchar(255) DEFAULT NULL,
   `avatar` varchar(255) DEFAULT NULL,
-  `birthday` date NOT NULL,
+  `birthday` datetime NOT NULL,
   `email` varchar(200) NOT NULL,
   `fullname` varchar(250) NOT NULL,
   `gender` varchar(255) NOT NULL,
@@ -27,52 +31,38 @@ CREATE TABLE `employee` (
   `department_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_6djefmjx26crewgafuih51sad` (`fullname`),
-  KEY `FKbejtwvg9bxus2mffsm3swj3u9` (`department_id`),
-  CONSTRAINT `FKbejtwvg9bxus2mffsm3swj3u9` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `refresh_token` (
-  `token_id` bigint NOT NULL AUTO_INCREMENT,
-  `created_at` datetime(6) DEFAULT NULL,
-  `created_by` varchar(255) DEFAULT NULL,
-  `is_active` bit(1) DEFAULT NULL,
-  `updated_at` datetime(6) DEFAULT NULL,
-  `updated_by` varchar(255) DEFAULT NULL,
-  `expiry_dt` datetime(6) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `user_id` bigint NOT NULL,
-  PRIMARY KEY (`token_id`),
-  UNIQUE KEY `UK_f95ixxe7pa48ryn1awmh2evt7` (`user_id`),
-  UNIQUE KEY `UK_r4k4edos30bx9neoq81mdvwph` (`token`),
-  UNIQUE KEY `UK_ga3cqp73wyumau3ghjfx7vq94` (`token`),
-  CONSTRAINT `FKfgk1klcib7i15utalmcqo7krt` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `FKbejtwvg9bxus2mffsm3swj3u9` (`department_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `role` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_at` datetime(6) DEFAULT NULL,
+  `id` bigint NOT NULL,
+  `created_at` datetime DEFAULT NULL,
   `created_by` varchar(255) DEFAULT NULL,
   `is_active` bit(1) DEFAULT NULL,
-  `updated_at` datetime(6) DEFAULT NULL,
+  `modified_at` datetime DEFAULT NULL,
+  `modified_by` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   `updated_by` varchar(255) DEFAULT NULL,
   `name` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_8sewwnpamngi6b1dwaa88askk` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `student` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL,
   `age` int NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `user` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_at` datetime(6) DEFAULT NULL,
+  `id` bigint NOT NULL,
+  `created_at` datetime DEFAULT NULL,
   `created_by` varchar(255) DEFAULT NULL,
   `is_active` bit(1) DEFAULT NULL,
-  `updated_at` datetime(6) DEFAULT NULL,
+  `modified_at` datetime DEFAULT NULL,
+  `modified_by` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   `updated_by` varchar(255) DEFAULT NULL,
   `avatar` varchar(255) DEFAULT NULL,
   `email` varchar(200) NOT NULL,
@@ -83,23 +73,22 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_ob8kqyqqgmefl0aco34akdtpe` (`email`),
   UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `user_role` (
   `user_id` bigint NOT NULL,
   `role_id` bigint NOT NULL,
   PRIMARY KEY (`user_id`,`role_id`),
-  KEY `FKa68196081fvovjhkek5m97n3y` (`role_id`),
-  CONSTRAINT `FK859n2jvi8ivhui0rl0esws6o` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FKa68196081fvovjhkek5m97n3y` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `FKa68196081fvovjhkek5m97n3y` (`role_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `role` (`id`, `created_at`, `created_by`, `is_active`, `updated_at`, `updated_by`, `name`) VALUES
-('1', '2020-09-26 09:13:56.421000', NULL, b'1', NULL, NULL, 'ADMIN'),
-('2', '2020-09-26 09:14:01.903000', NULL, b'1', NULL, NULL, 'USER');
 
-INSERT INTO `user` (`id`, `created_at`, `created_by`, `is_active`, `updated_at`, `updated_by`, `avatar`, `email`, `fullname`, `password`, `phone`, `username`) VALUES
-('1', '2020-09-26 09:14:06.544000', NULL, b'1', NULL, NULL, 'https://thumbs-prod.si-cdn.com/nnJARGtKrLypH4y3Vov2zGTG4xw=/fit-in/1600x0/filters:focal(554x699:555x700)/https://public-media.si-cdn.com/filer/a4/04/a404c799-7118-459a-8de4-89e4a44b124f/img_1317.jpg', 'admin@gmail.com', 'user', '$2a$10$kt.OP67e78I8OLnMIKBgeelahrRY1vRKTFu0iJAi0znGjmdgTB8si', '09784765976', 'admin');
+INSERT INTO `role` (`id`, `created_at`, `created_by`, `is_active`, `modified_at`, `modified_by`, `updated_at`, `updated_by`, `name`) VALUES
+('1', '2020-09-23 05:55:52', NULL, b'1', NULL, NULL, NULL, NULL, 'ADMIN');
+
+INSERT INTO `user` (`id`, `created_at`, `created_by`, `is_active`, `modified_at`, `modified_by`, `updated_at`, `updated_by`, `avatar`, `email`, `fullname`, `password`, `phone`, `username`) VALUES
+('1', '2020-09-23 05:58:08', NULL, b'1', NULL, NULL, NULL, NULL, 'https://thumbs-prod.si-cdn.com/nnJARGtKrLypH4y3Vov2zGTG4xw=/fit-in/1600x0/filters:focal(554x699:555x700)/https://public-media.si-cdn.com/filer/a4/04/a404c799-7118-459a-8de4-89e4a44b124f/img_1317.jpg', 'admin@gmail.com', 'admin admin', '$2a$10$FloFVLnBuEEoYmY2f/0mI.YVgXEbjhbABsh93o.1jbRoqEPRTPBem', '09784765976', 'admin');
 
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
 ('1', '1');
+
